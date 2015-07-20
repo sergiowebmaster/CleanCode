b08817b7fb0881110d6d831c09acc9f7226d0cff
sergio <sergioeduardo1981@gmail.com>
2015-07-20 02:10:13 -0300

    Classe responsável pelas ações referentes a diretórios.

---
+++ Dir.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ *@author Sérgio Eduardo Pinheiro Gomes <sergioeduardo1981@gmail.com>
+ */
+
+require_once 'CleanCodeClass.php';
+
+class Dir extends CleanCodeClass
+{
+	private static $aliases = array();
+	
+	private $path = '';
+	
+	function __construct($path)
+	{
+		$this->path = self::getPath($path);
+	}
+	
+	public static function addAlias($alias, $path)
+	{
+		self::$aliases[$alias] = $path;
+	}
+	
+	public static function getPath($alias)
+	{
+		if(strstr($alias, ':'))
+		{
+			$parts = explode(':', $alias);
+			return self::searchIn(self::$aliases, $parts[0]) . $parts[1];
+		}
+		else
+		{
+			return $alias;
+		}
+	}
+}
+?>
