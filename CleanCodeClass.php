a71099564ad7c314f441d0b9a6c2922d33cbd1e8
sergio <sergioeduardo1981@gmail.com>
2015-08-06 00:59:16 -0300

    Adição do método getRandomString(), para gerar Strings aleatórias.

--- CleanCodeClass.php
+++ CleanCodeClass.php
@@ -12,6 +12,20 @@ class CleanCodeClass
 		return $index? isset($array[$index])? $array[$index] : $default : $array;
 	}
 	
+	protected static function getRandomString($size)
+	{
+		$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$string = '';
+		
+		for ($i = 0; $i < $size; $i++)
+		{
+			$c = rand(0, strlen($characters));
+			$string .= $characters[$c - 1];
+		}
+		
+		return $string;
+	}
+	
 	protected static function parseVar($text)
 	{
 		$parts	= preg_split('/[^a-z0-9]/i', $text, 0, PREG_SPLIT_NO_EMPTY);
